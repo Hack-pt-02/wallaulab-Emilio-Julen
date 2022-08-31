0453651e1287e60e2177e978979753c9839f491a
Julen <101423308+Julen384@users.noreply.github.com>
2022-08-31 21:08:14 +0200

    Added Bootstrap again

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,9 @@ window._ = _;
  */
 
 import axios from 'axios';
+import '../css/app.css';
+import * as bootstrap from 'bootstrap';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';

855a219f077ed35d3d3f81732dd877e72303d91e
Emilio Vargas Millan <emiliovm@outlook.es>
2022-09-01 18:40:34 +0200

    con ediciones de rama

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,9 @@
 <div>
     <h1>lo mejor</h1>
+
+    allSubdomainsOfApplicationUrladsf
+    AWS_DEFAULT_REGIONasdf
+    allSubdomainsOfApplicationUrladsfasdf
+    allSubdomainsOfApplicationUrladsfasdf
+    
 </div>

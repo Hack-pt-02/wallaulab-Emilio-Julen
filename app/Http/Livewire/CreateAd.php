b6a84a414de46c429d4ad1a37b3def451e7d345f
Julen <101423308+Julen384@users.noreply.github.com>
2022-09-01 20:44:51 +0200

    Changes

--- app/Http/Livewire/CreateAd.php
+++ app/Http/Livewire/CreateAd.php
@@ -3,9 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Ad;
 
 class CreateAd extends Component
 {
+
+    public $title;
+    public $body;
+    public $price;
+    
+    public function store()
+    {
+        Ad::create([
+            'title'=>$this->title,
+            'body'=>$this->body,
+            'price'=>$this->price,
+        ]);
+        $this->cleanForm();
+    }
+
+    public function cleanForm()
+    {
+        $this->title = "";
+        $this->body = "";
+        $this->price = "";
+    }
+
     public function render()
     {
         return view('livewire.create-ad');

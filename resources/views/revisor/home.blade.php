e04e1f5c83add2a8b48e3bf0f92f93d8354b57ee
Julen <101423308+Julen384@users.noreply.github.com>
2022-09-13 20:29:21 +0200

    bug fixed

--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -85,6 +85,6 @@
         </div>
     </div>
     @else
-        <h3 class="text-center">{{ _("No hay anuncios para revisar, vuelve más tarde, gracias") }}</h3>
+        <h3 class="text-center">{{ __("No hay anuncios para revisar, vuelve más tarde, gracias") }}</h3>
     @endif
 </x-layout>

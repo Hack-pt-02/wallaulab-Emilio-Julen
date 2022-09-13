b084df9fd04dd8d561526fc4acf3d033bdf76e82
Emilio Vargas Millan <emiliovm@outlook.es>
2022-09-13 19:47:12 +0200

    translated register, footer & home revisor

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 col-md-6">
-                Powered by hackademy
+                {{ __("Desarrollado por") }}
             </div>
             <div class="col-12 col-md-6 text-end">
-                <a href="#" class="text-white text-decoration-none">Policy</a>
-                <a href="{{route('revisor.become')}}" class="btn btn-info">Work with us</a>
+                <a href="#" class="text-white text-decoration-none">{{ __("Política de privacidad") }}</a>
+                <a href="{{route('revisor.become')}}" class="btn btn-info">{{ __("Trabaja con nosotros") }}</a>
                 <a href="#" class="text-white text-decoration-none">Facebook</a>
                 <a href="#" class="text-white text-decoration-none">Instagram</a>
             </div>

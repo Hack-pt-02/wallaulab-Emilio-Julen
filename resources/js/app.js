ad00aa7f5ab0d0ff1eca220f2b09bd8e5e5dd7e6
Emilio Vargas Millan <emiliovm@outlook.es>
2022-10-01 00:52:06 +0200

    with sweetAlert in adCreate working correctly

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,10 @@
 import './bootstrap';
-@import 'boxicons';
 
+/** Primer importamos Swal
+ * Luego parece ser que traemos Swal a todo el browser con window.Swal
+ * Entonces es reconocido por el session->flash
+ */
+
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+//import 'boxicons';
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,8 +16,6 @@
     @livewireStyles
     {{$style ?? ''}}
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-
-
 </head>
 
 <body>

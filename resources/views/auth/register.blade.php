6f34813d42ebdb4c4226ec09efe6c1471b290056
Emilio Vargas Millan <emiliovm@outlook.es>
2023-01-21 23:26:07 +0100

    con mensaje de datos reales registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,12 @@
 <x-layout>
     <x-slot name="title">Wallaulab - Register</x-slot>
     <!-- ======= REGISTER ======= -->
-    <div class="container-fluid bg-accent vh-100">
+    <div class="container-fluid bg-accent">
         <div class="row mb-5 pb-5">
             <div class="col-12">
                 <div class="d-flex flex-column align-items-center ">
                     <div class="form-content w-50 justify-content-center mb-5 pb-5">
+                        <p class="alert alert-warning my-2">Esta web es una demo, <span class="fw-bold">¡no introducir datos reales!</p>
                         <!--FORM TITLE -->
                         <div class="section-title">
                             <h2 class="form-title space-around">{{ __('Crear tu cuenta') }}
@@ -52,14 +53,12 @@
                                 <div class="validate"></div>
                             </div>
                             <!--Button-Register-->
-                            <button type="submit" class=" btn btn-primary form-button-edit text-center space-around my-2">
-                                {{ __("Registrar") }}
-                            </button>
+                            <div class="d-flex justify-content-center">
+                                <button type="submit" class=" btn btn-primary form-button-edit text-center space-around my-2">
+                                    {{ __("Registrar") }}
+                                </button>
+                            </div>
                         </form>
-                    </div>
-                    <div class="form-link mt-4 d-flex">
-                        <p class="text-white">{{ __("¿Ya eres de los nuestros?") }}</p>
-                        <a class="text-reset text-decoration-none ps-2" href="{{ route('login') }}"><u>{{ __("¡Entra ya!") }}</u></a>
                     </div>
                 </div>
             </div>
